a8d84b94fca73e2bf9def26ee682834006388e00
C360 <c360@C360s-MacBook-Pro.local>
2023-12-26 21:11:44 +0530

    fixed issue

--- src/App.js
+++ src/App.js
@@ -22,25 +22,65 @@ const App = () => {
   const userVideo = useRef();
   const connectionRef = useRef();
 
+  // useEffect(() => {
+  //   navigator.mediaDevices.getUserMedia({ video: true, audio: true }).then((stream) => {
+  //     setStream(stream);
+  //     if (myVideo.current) {
+  //       myVideo.current.srcObject = stream;
+  //     }
+  //   });
+
+  //   socket.on('me', (id) => {
+  //     setMe(id);
+  //   });
+
+  //   socket.on('calluser', (data) => {
+  //     setReceivingCall(true);
+  //     setCaller(data.from);
+  //     setName(data.name);
+  //     setCallerSignal(data.signal);
+  //   });
+  // }, []);
+
   useEffect(() => {
-    navigator.mediaDevices.getUserMedia({ video: true, audio: true }).then((stream) => {
-      setStream(stream);
-      if (myVideo.current) {
-        myVideo.current.srcObject = stream;
+    const getUserMediaAndSetStream = async () => {
+      try {
+        const stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
+        setStream(stream);
+  
+        if (myVideo.current) {
+          myVideo.current.srcObject = stream;
+        }
+      } catch (error) {
+        console.error('Error accessing media devices:', error);
       }
-    });
-
-    socket.on('me', (id) => {
-      setMe(id);
-    });
-
-    socket.on('calluser', (data) => {
-      setReceivingCall(true);
-      setCaller(data.from);
-      setName(data.name);
-      setCallerSignal(data.signal);
-    });
-  }, []);
+    };
+  
+    // Set up socket event listeners
+    const setupSocketListeners = () => {
+      socket.on('me', (id) => {
+        setMe(id);
+      });
+  
+      socket.on('calluser', (data) => {
+        setReceivingCall(true);
+        setCaller(data.from);
+        setName(data.name);
+        setCallerSignal(data.signal);
+      });
+    };
+  
+    getUserMediaAndSetStream();
+    setupSocketListeners();
+  
+    // Clean up the event listeners when the component unmounts
+    return () => {
+      socket.off('me');
+      socket.off('calluser');
+    };
+  
+  }, [socket]); // Dependency on socket to ensure it's in the dependency array
+  
 
   const callUser = (id) => {
     const peer = new SimplePeer({
